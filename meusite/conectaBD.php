f39fd7efd3bdf5304969560c14bed9a50553c76d
ileanakaori <kaorikuroda1999@gmail.com>
2026-06-30 22:49:24 -0300

    Create conectaBD.php

---
+++ meusite/conectaBD.php
@@ -0,0 +1,18 @@
+<?php
+    // Dados de conexão
+    $servername = "localhost:3306"; 
+    $username   = "root"; 
+    $password   = ""; 
+    $database   = "lojaDeLivros";
+
+    // Cria conexão
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Verifica conexão
+    if ($conn->connect_error) {
+        die("Falha na conexão: " . $conn->connect_error);
+    } else {
+        // Opcional: mensagem de sucesso (remova em produção)
+        // echo "Conectado com sucesso!";
+    }
+?>
